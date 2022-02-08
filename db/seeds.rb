8987606dab6165e9f5ca5249b69c63ef22e574a6
Chris Bolas <tanlaan33@gmail.com>
2022-02-08 10:51:25 -0800

    Add db seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Kitten.destroy_all # HOW COULD YOU?!
+
+Kitten.create({
+  name: 'Timmy',
+  age: 1,
+  cuteness: 8,
+  softness: 9
+})
+
+Kitten.create({
+  name: 'Tammy',
+  age: 8,
+  cuteness: 7,
+  softness: 10
+})
+
+Kitten.create({
+  name: 'Ben',
+  age: 13,
+  cuteness: 8,
+  softness: 7
+})
