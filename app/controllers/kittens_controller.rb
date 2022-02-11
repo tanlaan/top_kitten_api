7450c3494464f1348b9ef023a84cccd8b1a56729
Chris Bolas <tanlaan33@gmail.com>
2022-02-11 10:09:37 -0800

    Base resource functionality for Kittens

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,22 +1,54 @@
 class KittensController < ApplicationController
+  before_action :get_kitten, only: [:show, :edit, :destroy]
+
   def index
+    @kittens = Kitten.all
   end
 
   def show
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def edit
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to @kitten, notice: 'Kitten created successfully.'
+    else
+      render :new, notice: 'Something went wrong.'
+    end
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+    @kitten.update(kitten_params)
+    if @kitten.save
+      redirect_to @kitten, notice: 'Kitten updated successfully.'
+    else
+      render :edit, notice: 'Something went wrong.'
+    end
   end
 
   def destroy
+    if @kitten.destroy
+      redirect_to kittens_url, notice: 'Kitten destroyed. How could you?!'
+    else
+      redirect_to kittens_url, notice: 'Kitten not found in database.'
+    end
+  end
+
+  private
+
+  def get_kitten
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
